1f947b4672243a8192028f405541d6a35d2d7d21
DrEverything <c.petaroudis@icloud.com>
2022-02-04 16:11:29 +0200

    0

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function eight() {
+    var currentOperand = document.getElementById("currentOperand");
+    if (currentOperand.innerHTML != "0") {
+        currentOperand.innerHTML += "8";
+    } else {
+        currentOperand.innerHTML = "8";
+    }
+    
+}
+
