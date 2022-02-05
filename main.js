abf58957229a876bbe8b1021fce2d67c0d116929
DrEverything <c.petaroudis@icloud.com>
2022-02-05 16:16:37 +0200

    0

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let numbers = document.getElementsByClassName("number");
+var isPointed = false;
 
 for (i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener("click", function () {
@@ -11,3 +12,18 @@ for (i = 0; i < numbers.length; i++) {
         }
     });
 }
+
+document.getElementById("point").addEventListener("click", function () {
+    if (isPointed == false) {
+        document.getElementById("currentOperand").innerHTML += ".";
+
+        isPointed = true;
+    }
+});
+
+document.getElementById("allClear").addEventListener("click", function () {
+    document.getElementById("currentOperand").innerHTML = "0";
+    document.getElementById("previousOperand").innerHTML = "0";
+
+    isPointed = false;
+});
