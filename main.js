e3b4adb68288aee8fac4347ab414357b8df301bd
DrEverything <c.petaroudis@icloud.com>
2022-02-05 04:01:00 +0200

    0

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
+let numbers = document.getElementsByClassName("number");
 
+for (i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function () {
+        var currentOperand = document.getElementById("currentOperand");
 
-function eight() {
-    var currentOperand = document.getElementById("currentOperand");
-    if (currentOperand.innerHTML != "0") {
-        currentOperand.innerHTML += "8";
-    } else {
-        currentOperand.innerHTML = "8";
-    }
-    
+        if (currentOperand.innerHTML != "0") {
+            currentOperand.innerHTML += this.innerText;
+        } else {
+            currentOperand.innerHTML = this.innerText;
+        }
+    });
 }
-
