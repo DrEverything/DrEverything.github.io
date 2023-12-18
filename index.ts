4220708912be35943cf53eba7ed8c7a15afc6495
DrEverything <c.petaroudis@icloud.com>
2023-12-18 14:56:15 +0200

    1

--- index.ts
+++ index.ts
@@ -1,18 +1,15 @@
 var canvas = document.querySelector('canvas') as HTMLCanvasElement;
-const gl = canvas.getContext('webgl2');
-if (!gl) {
-    console.error('Unable to initialize WebGL. Your browser may not support it.');
-}
-canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 window.onresize = () => {
-    canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
 // Vertex Shader
-const vsSource = `
-    attribute vec4 aVertexPosition;
+const vsSource = `#version 300 es
+
+    in vec4 aVertexPosition;
 
     void main(void) {
         gl_Position = vec4(aVertexPosition.xy, .0, 1.);
@@ -20,13 +17,57 @@ const vsSource = `
 `;
 
 // Fragment Shader
-const fsSource = `
-    precision mediump float;
+const fsSource = `#version 300 es
 
-    void main(void) {
-        gl_FragColor = vec4(.0, 1.0, 1.0, .5);
-    }
+precision mediump float;
+
+uniform float iTime;
+uniform vec2 iResolution;
+out vec4 fragColor;
+
+    float aaa(vec2 position, float radius) {
+  return step(radius, length(position - vec2(0.5)));
+}
+
+vec3 wave(vec2 uv) {
+  float curve = .3 * sin((9.25 * uv.x) + (2. * iTime));
+  float lineAShape =
+      smoothstep(1. - clamp(distance(curve + uv.y, .0), 0., 1.), 1., .998);
+  return (1. - lineAShape) *
+         vec3(mix(vec3(.8, .2, .2), vec3(.5, .2, .2), lineAShape));
+}
+
+void main(void) {
+  vec2 uv = (2. * gl_FragCoord.xy - iResolution.xy) / iResolution.y;
+// vec2 uv = (gl_FragCoord.xy / vec2(gl_FragCoord.w, gl_FragCoord.w)) * 2.0 - 1.0;
+
+  vec3 color = vec3(0.);
+  float color_w = 1.;
+  
+//   if (uv.x > 0.) {
+//     color.x = 1.;
+//   }
+
+  color = wave(uv);
+//   vec2 d = vec2(abs(uv.x-.1), abs(uv.y - .5)) - .3;
+//   float square = length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+//   float circle = length(uv + .4) - .4;
+//   float squareColor = step(square, 0.02);
+
+//   // Use smoothstep() to create the circle
+//   float circleColor = smoothstep(0., 0.9, circle);
+
+//   // Combine the square and circle colors
+//   color += vec3(squareColor, circleColor, 0.);
+  
+  fragColor = vec4(color, color_w);
+}
 `;
+
+const gl = canvas.getContext('webgl2');
+if (!gl) {
+    console.error('Unable to initialize WebGL. Your browser may not support it.');
+}
 
 function compileShader(gl: WebGL2RenderingContext, source: string, type: GLenum): WebGLShader | null {
     const shader = gl.createShader(type);
@@ -85,7 +126,6 @@ gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
 const positionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
 const vertexBuffer = gl.createBuffer();
-
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 const vertices = new Float32Array([
     -1.0, 1.0, // Top left
@@ -95,8 +135,26 @@ const vertices = new Float32Array([
 ]);
 gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
+let iTimeLocation = gl.getUniformLocation(shaderProgram, "iTime");
+let iResolutionLocation = gl.getUniformLocation(shaderProgram, "iResolution");
+let time = 0.;
+gl.uniform1f(iTimeLocation, time);
+gl.uniform2f(iResolutionLocation, canvas.width, canvas.height);
+
 gl.enableVertexAttribArray(positionAttribute);
 gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
 
 gl.clear(gl.COLOR_BUFFER_BIT);
 gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 2);
+
+function GLDraw() {
+    time += 0.02;
+    gl.uniform1f(iTimeLocation, time);
+    gl.uniform2f(iResolutionLocation, canvas.width, canvas.height);
+    
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertices.length / 2);
+    requestAnimationFrame(GLDraw);
+}
+
+GLDraw();
