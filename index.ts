5c3eae16242510126c8e69bf117bb5e15736bdff
DrEverything <c.petaroudis@icloud.com>
2024-01-02 22:13:20 +0200

    1

--- index.ts
+++ index.ts
@@ -160,37 +160,6 @@ Promise.all(shaderPromises)
         for (let i = 0; i < canvases.length; i++) {
             initWebGL2(canvases[i], shaders[0], shaders[i + 1]);
         }
-    });
+});
     
-// if (navigator.gpu) {
-//     const adapter = await navigator.gpu.requestAdapter();
-//     if (!adapter) {
-//         console.error("No appropriate GPUAdapter found.");
-//     } else {
-//         const device = await adapter.requestDevice();
-//         canvases[0].width = canvases[0].clientWidth;
-//         canvases[0].height = canvases[0].clientHeight;
-//         const context = canvases[0].getContext("webgpu");
-//         const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
-//         context.configure({
-//             device: device,
-//             format: canvasFormat,
-//         });
-//         const encoder = device.createCommandEncoder();
-//         const pass = encoder.beginRenderPass({
-//             colorAttachments: [{
-//                 view: context.getCurrentTexture().createView(),
-//                 loadOp: "clear",
-//                 storeOp: "store",
-//             }]
-//         });
-//         pass.end();
-//         const commandBuffer = encoder.finish();
-//         device.queue.submit([commandBuffer]);
-//         device.queue.submit([encoder.finish()]);
-//     }
-// } else {
-//     console.error("WebGPU not supported on this browser.");
-// }
-
 export { }
