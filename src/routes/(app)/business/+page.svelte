0ca44db01f09180bfef69d286dee356ea7b4a3fa
DrEverything <c.petaroudis@icloud.com>
2026-05-23 02:55:04 +0300

    Whatever

--- src/routes/(app)/business/+page.svelte
+++ src/routes/(app)/business/+page.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-  import SiteHeader from "$lib/components/site-header.svelte";
-  import SectionCards from "$lib/components/section-cards.svelte";
-  import ChartAreaInteractive from "$lib/components/chart-area-interactive.svelte";
-  import LayoutGrid from "@tabler/icons-svelte/icons/grid-dots";
-  import * as Popover from "$lib/components/ui/popover";
-
-  import { buttonVariants } from "$lib/components/ui/button/index.js";
-
+  import Dashboard from "./Dashboard.svelte";
 </script>
 
-
+<Dashboard />
